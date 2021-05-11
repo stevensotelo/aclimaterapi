f7a85b4def0a19e0a51f7063b28b62579b4360dd
stevensotelo <stevenbetancurt@hotmail.com>
2021-05-11 09:48:20 -0500

    first commit

---
+++ R/catalog.R
@@ -0,0 +1,34 @@
+#' Get weather stations
+#'
+#' @description function which gets a list of all weather stations available into the aclimate platform.
+#'
+#' @param url_root Url root where the API is located.
+#'
+#' @return A data.frame, with the list of all weather stations.
+#'
+#' @examples
+#' url_root = "https://pronosticosapi.aclimatecolombia.org/api/"
+#' df_ws = get_ws(url_root)
+#' print(head(df_ws))
+#'
+#' @export
+get_ws = function(url_root){    
+    # Setting certificates
+    httr::set_config(config(ssl_verifypeer = 0L))
+    # Downloading data
+    url_geographic = paste0(url,"Geographic/json")
+    request = GET(url)
+    response = content(request, as = "text", encoding = "UTF-8")
+    data = fromJSON(response)
+    df = do.call(rbind,  
+                    lapply(data,function(s){
+                        do.call(rbind,lapply(s$municipalities,function(m){
+                            do.call(rbind,lapply(m$weather_stations,function(w){
+                                data.frame(country=s$country, st_id=s$id, st_name=s$name, 
+                                            mu_id=m$id, mu_name=m$name, 
+                                            ws_id=w$id, ws_ext_id=w$ext_id, ws_name=w$name, ws_origin=w$origin, ws_lat=w$latitude, ws_lon=w$longitude)
+                            }))
+                        }))
+                    }))
+    return (df)
+}
---
+++ R/forecast.R
@@ -0,0 +1,56 @@
+#' Get climate forecast
+#'
+#' @description function which gets the forecast for a set of weather stations available into the aclimate platform
+#'
+#' @param url_root Url root where the API is located.
+#' @param stations Array of strings with the ids of the weather stations which want to search.
+#'
+#' @return A List with 3 attributes (probabilities, performance, scenarios).
+#'
+#' @examples
+#' url_root = "https://pronosticosapi.aclimatecolombia.org/api/"
+#' stations=c("58504f1a006cb93ed40eebe2","58504f1a006cb93ed40eebe3")
+#' obj_f = get_forecast_climate(url_root, stations)
+#' print(obj_f)
+#'
+#' @export
+get_forecast_climate = function(url_root, stations){
+    # Setting certificates
+    httr::set_config(config(ssl_verifypeer = 0L))
+    # Download data
+    ws = paste(stations,collapse=",")
+    url = paste0(url_root,"Forecast/Climate/",ws,"/true/json")
+    request = GET(url)
+    response = content(request, as = "text", encoding = "UTF-8")
+    data = fromJSON(response)
+    df_prob = do.call(rbind,  
+                    lapply(data$climate,function(w){
+                        do.call(rbind,lapply(w$data,function(wd){
+                            do.call(rbind,lapply(wd$probabilities,function(p){
+                                data.frame(ws_id=w$weather_station, year=wd$year, month=wd$month, 
+                                           measure=p$measure, lower=p$lower, normal=p$normal, upper=p$upper)
+                            }))
+                        }))
+                    }))
+    df_perf = do.call(rbind,  
+                    lapply(data$climate,function(w){
+                        do.call(rbind,lapply(w$performance,function(p){
+                            data.frame(ws_id=w$weather_station, 
+                                       year=p$year, month=p$month, 
+                                       measure=p$measure, value=p$value)
+                            
+                        }))
+                    }))
+    df_scen = do.call(rbind,  
+                    lapply(data$scenario,function(w){
+                        do.call(rbind,lapply(w$monthly_data,function(wm){
+                            do.call(rbind,lapply(wm$data,function(d){
+                                data.frame(ws_id=w$weather_station, scenario=w$name, 
+                                           year=w$year, month=wm$month, 
+                                           measure=d$measure, value=d$value)
+                            }))
+                        }))
+                    }))
+    forecast_climate = list(probabilities=df_prob, performance=df_perf, scenarios=df_scen)
+    return (forecast_climate)
+}
