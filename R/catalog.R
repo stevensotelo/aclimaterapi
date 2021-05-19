22965eac5b2ab1f7d7af1021d7dbf901b3120f9e
stevensotelo <stevenbetancurt@hotmail.com>
2021-05-19 15:06:57 -0500

    added method to get agronomy data

--- R/catalog.R
+++ R/catalog.R
@@ -8,8 +8,8 @@
 #'
 #' @examples
 #' url_root = "https://pronosticosapi.aclimatecolombia.org/api/"
-#' df_ws = get_ws(url_root)
-#' print(head(df_ws))
+#' df = get_ws(url_root)
+#' print(head(df))
 #'
 #' @export
 get_ws = function(url_root){   
@@ -32,4 +32,46 @@ get_ws = function(url_root){
                         }))
                     }))
     return (df)
+}
+
+#' Get agronomy setup
+#'
+#' @description function which gets a list of setups of cultivars and soils available into the aclimate platform.
+#'
+#' @param url_root Url root where the API is located.
+#'
+#' @return A List with 2 attributes (cultivars, soils).
+#'
+#' @examples
+#' url_root = "https://pronosticosapi.aclimatecolombia.org/api/"
+#' df = get_agronomy(url_root)
+#' print(head(df))
+#'
+#' @export
+get_agronomy = function(url_root){   
+    library(httr)
+    library(rjson)
+    httr::set_config(config(ssl_verifypeer = 0L))     
+    # Downloading data
+    url = paste0(url_root,"Agronomic/true/json")
+    request = GET(url)
+    response = content(request, as = "text", encoding = "UTF-8")
+    data = fromJSON(response)
+    df_soils = do.call(rbind,  
+                    lapply(data,function(c){
+                        do.call(rbind,lapply(c$soils,function(s){
+                            data.frame(crop_id=c$cp_id,crop_name=c$cp_name, 
+                                        soil_id=s$id, soil_name=s$name)
+                        }))
+                    }))
+    df_cultivars = do.call(rbind,  
+                    lapply(data,function(c){
+                        do.call(rbind,lapply(c$cultivars,function(cu){
+                            data.frame(crop_id=c$cp_id,crop_name=c$cp_name, 
+                                        cultivar_id=cu$id, cultivar_name=cu$name,
+                                        cultivar_rainfed=cu$rainfed, cultivar_national=cu$national)
+                        }))
+                    }))
+    agronomy = list(cultivars=df_cultivars, soils=df_soils)
+    return (agronomy)
 }
